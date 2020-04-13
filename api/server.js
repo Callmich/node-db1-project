192a10eb6d0ee08ecbc3902d6f0b854f79705813
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-13 15:53:35 -0600

    added info to the router and now get for router is pulling all of the accounts

--- api/server.js
+++ api/server.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const db = require("../data/dbConfig.js");
 
+const Router = require('../router/router.js')
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', Router)
+
+server.get('/', (req, res)=>{
+    res.status(200).json({ api: "up"})
+})
 
 module.exports = server;
