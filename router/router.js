726dc091a158fcccc48766b958711814bd312450
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-13 16:24:22 -0600

    The get action for spwcific IDs has been created and also shows a dynamic 404 error if there is no account assosiated with the ID

--- router/router.js
+++ router/router.js
@@ -15,4 +15,34 @@ router.get("/", (req, res)=>{
     })
 })
 
+router.get("/:id", (req, res)=>{
+    db('accounts')
+    .where('id', req.params.id)
+    .first()
+    .then(account =>{
+        if (account){
+          res.status(200).json(account)
+        } else{
+            res.status(404).json({message: `The account with ID ${req.params.id} does not exist`})
+        }
+        
+    })
+    .catch(error=>{
+        console.log(error)
+        res.status(500).json({error: "The server could not retrieve this account"})
+    })
+})
+
+router.post("/", (req, res)=>{
+
+})
+
+router.patch("/:id", (req, res)=>{
+
+})
+
+router.delete("/:id", (req, res)=>{
+
+})
+
 module.exports = router;
